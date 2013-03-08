4e645628f6f8ed05c70ed9e32c25d7a6699fb2fd
Ralf Kistner <ralf.kistner@gmail.com>
2013-03-08 18:44:49 +0200

    Working MIDI!

--- Organ.ino
+++ Organ.ino
@@ -5,13 +5,37 @@
  */
 // pins 15~17 to GND, I2C bus address is 0x20
 #include "Wire.h"
+#define MIDI_AUTO_INSTANCIATE    0
+#include <MIDI.h>
+
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial, MyMIDI)
 
 byte last[12];
 byte counter[12];
 
+const int DOWN = 0b01;
+const int UP = 0b10;
+
+const byte NOTE_MAPPING[12] = {59, 57, 55, 53, 51, 49, 52, 50, 54, 56, 58, 60};
+
+/*
+Note ID MIDI
+C# 5  49
+D  7  50
+D# 4  51
+E  6  52
+F  3  53
+F# 8  54
+G  2  55
+G# 9  56
+A  1  57
+A# 10 58
+B  0  59
+C  11 60
+*/
+
 void setup()
 {
-    Serial.begin(9600);
     Wire.begin(); // wake up I2C bus
     TWBR=2;  // Increase the speed - http://electronics.stackexchange.com/questions/29457/how-to-make-arduino-do-high-speed-i2c
     
@@ -25,6 +49,9 @@ void setup()
     Wire.write(0x01); // IODIRB register
     Wire.write(0b00000011); // set all of bank B to inputs
     Wire.endTransmission();
+    
+    MyMIDI.begin(4);          // Launch MIDI and listen to channel 4
+    Serial.begin(115200);
 }
 void loop()
 {
@@ -57,17 +84,23 @@ void loop()
         if(pressed == 0b11) {
             counter[i] += 1;
         } else {
-            if(pressed != last[i]) {  // || (pressed == 0b11 && i >= 5)
-              Serial.print(i);
-              Serial.print(' ');
-              if(pressed == 0b01) {
-                 Serial.print("DOWN");
-              } else {
-                  Serial.print("UP");
-              }
-              Serial.print(' ');
-              Serial.print(counter[i]);
-              Serial.println();
+            if(pressed != last[i]) {
+               byte note = NOTE_MAPPING[i];
+               if(pressed == DOWN) {
+                   MyMIDI.sendNoteOn(note, 64, 1);  // Send a Note (pitch 42, velo 127 on channel 1)
+               } else {
+                   MyMIDI.sendNoteOff(note, 64, 1);
+               }
+//              Serial.print(i);
+//              Serial.print(' ');
+//              if(pressed == 0b01) {
+//                 Serial.print("DOWN");
+//              } else {
+//                  Serial.print("UP");
+//              }
+//              Serial.print(' ');
+//              Serial.print(counter[i]);
+//              Serial.println();
             }
             counter[i] = 0;
             last[i] = pressed;
